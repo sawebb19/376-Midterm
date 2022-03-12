083fb4b3a061c182169b9004bfe9a1859e5d227d
Jason <j.truskowski@comcast.net>
2022-03-12 14:54:45 -0500

    Add test level file

---
+++ assets/Midterm.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Midterm" tilewidth="32" tileheight="32" tilecount="768" columns="32">
+ <image source="spritemap.png" width="1024" height="768"/>
+</tileset>
