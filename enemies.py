93aeaf6954eedd920a58f17c75ac542eb35213e8
sawebb19 <webbsamu@mail.gvsu.edu>
2022-03-13 19:10:55 -0400

    Fix enemy movement

--- enemies.py
+++ enemies.py
@@ -9,18 +9,16 @@ import pygame as pg
 class Enemy(DUGameObject):
     def __init__(self, eng, x=0, y=0):
         super().__init__()
-        self.images = []
-        self.images.append([])
-        self.images.append([])
+        self.images = [[],[]] # [0][x] for facing left, [1][x] for facing right
         for i in range(1,11):
-            self.images[0].append(pg.image.load("./zombie/Walk ("+str(i)+").png").convert_alpha())
-            self.images[0][i-1] = self.images[0][i-1].subsurface(self.images[0][i-1].get_bounding_rect())
-            self.images[0][i-1] = pg.transform.scale(self.images[0][i-1], (128, 128)) 
-            self.images[1].append(pg.transform.flip(self.images[0][i-1], True, False))
-        self.image = self.images[0][0]
+            self.images[1].append(pg.image.load("./zombie/Walk ("+str(i)+").png").convert_alpha())
+            self.images[1][i-1] = self.images[1][i-1].subsurface(self.images[1][i-1].get_bounding_rect())
+            self.images[1][i-1] = pg.transform.scale(self.images[1][i-1], (128, 128)) 
+            self.images[0].append(pg.transform.flip(self.images[1][i-1], True, False))
+        self.image = self.images[1][0]
         self.rect = self.image.get_rect()
         self.dirty = 2
-        self.direction = -1
+        self.direction = 0 # 0 for left, 1 for right
         self.speed = -1
         self.x = x
         self.y = y
@@ -37,16 +35,15 @@ class Enemy(DUGameObject):
             self.current_frame = (self.current_frame + 1 ) % 10
             self.walk_time = 0
         self.image = self.images[self.direction][self.current_frame]
-        dir = 1
-        if self.direction == 1:
-            dir = -1
+
         # FIXME: flip sprite, go left when right edge of sprite hits right edge of screen
-        if (self.x <= 0 and dir == 1) or self.x >= self.eng.width:
-            self.direction = self.direction * -1
-            dir *= -1
-        self.x = self.x + 100 * dir * self.speed * self.eng.deltaTime
+        if (self.x <= 0 or self.x >= self.eng.width - 128):
+            self.direction = abs(self.direction - 1)
+        self.x = self.x + 100 * (self.direction - 0.5) * -2 * self.speed * self.eng.deltaTime
+        
         self.rect.x = self.x
         self.rect.y = self.y
+        
         for event in self.eng.events:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_a:
@@ -54,5 +51,6 @@ class Enemy(DUGameObject):
                 if event.key == pg.K_d:
                     self.direction = 0
                     self.image = pg.transform.flip(self.image, True, False)
+
         pg.draw.rect(self.image, (0, 0, 255), self.image.get_bounding_rect(), width=1)
         pg.draw.rect(self.image, (255, 0, 0), self.image.get_rect(), width=1)
